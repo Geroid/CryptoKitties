e79a431a0fe05c057d868d368a111106b001be97
Viktor Rezvantsev <viktor.rezvantsev@umbrellait.com>
2022-11-26 09:12:07 +0300

    Initial Commit

---
+++ CryptoKitties/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoKitties
+//
+//  Created by Viktor Rezvantsev on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
